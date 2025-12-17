c291ee84deb6e23719a2d424af8cccaeb2d1ea91
Shreya VR <shreyarangachar@gmail.com>
2025-12-17 10:23:05 +0530

    chore:

--- dags/stock_dag.py
+++ dags/stock_dag.py
@@ -192,4 +192,17 @@ with DAG(
         provide_context=True,
     )
 
+
+
+
+
+    create_table_task=PythonOperator(
+        task_id = "create_table"
+        python_callable = fetch_stock_data,
+        provide_contect =False
+
+    )
+
     create_table_task >> fetch_task >> store_task
+
+
